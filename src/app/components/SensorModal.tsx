523373bb44adc4ce083faad5d5157705da315627
David Rotariu <davidrotariu139@gmail.com>
2024-12-05 16:44:58 +0200

    Implemented modal with hardcoded data

--- src/app/components/SensorModal.tsx
+++ src/app/components/SensorModal.tsx
@@ -1,8 +1,8 @@
 'use client';
 
-import { Modal } from '@mantine/core';
+import { Modal, BackgroundImage, Center, Box, Title } from '@mantine/core';
 import { useDisclosure } from '@mantine/hooks';
-import { useState, useEffect } from 'react';
+import { useEffect } from 'react';
 
 interface SensorModalProps {
     sensor: number;
@@ -34,7 +34,45 @@ const SensorModal = ({ sensor, setSensor }: SensorModalProps) => {
                 withOverlay={false}
                 className="pl-[550px]"
             >
-                <div>14°C</div>
+                <Box px="xl" py="sm" mx="auto">
+                    <BackgroundImage src="/clouds.jpg" radius="md">
+                        <Center p="sm">
+                            {sensor == 1 && <Title c="white">24°C</Title>}
+                            {sensor == 2 && <Title c="white">19°C</Title>}
+                            {sensor == 3 && <Title c="white">22°C</Title>}
+                        </Center>
+                    </BackgroundImage>
+                </Box>
+
+                <Box px="xl" py="sm" mx="auto">
+                    <BackgroundImage src="/water.jpg" radius="md">
+                        <Center p="sm">
+                            {sensor == 1 && <Title c="white">94%</Title>}
+                            {sensor == 2 && <Title c="white">97%</Title>}
+                            {sensor == 3 && <Title c="white">100%</Title>}
+                        </Center>
+                    </BackgroundImage>
+                </Box>
+
+                <Box px="xl" py="sm" mx="auto">
+                    <BackgroundImage src="/noise.jpg" radius="md">
+                        <Center p="sm">
+                            {sensor == 1 && <Title c="white">73dB</Title>}
+                            {sensor == 2 && <Title c="white">120dB</Title>}
+                            {sensor == 3 && <Title c="white">40dB</Title>}
+                        </Center>
+                    </BackgroundImage>
+                </Box>
+
+                <Box px="xl" py="sm" mx="auto">
+                    <BackgroundImage src="/dust.jpg" radius="md">
+                        <Center p="sm">
+                            {sensor == 1 && <Title c="white">67</Title>}
+                            {sensor == 2 && <Title c="white">65</Title>}
+                            {sensor == 3 && <Title c="white">69</Title>}
+                        </Center>
+                    </BackgroundImage>
+                </Box>
             </Modal>
         </div>
     );
