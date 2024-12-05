bf673022d5787ea97d89e95e9d96a74178262d95
David Rotariu <davidrotariu139@gmail.com>
2024-12-05 17:13:51 +0200

    Added floating green button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,24 @@
 'use client';
 
+import { Button } from '@mantine/core';
 import Map from './components/Map';
+import Image from 'next/image';
 
 const Home = () => {
     return (
         <div>
-            <div className="width-60vh">
-                <Map />
+            <Map />
+            <div className="fixed bottom-10 right-10 text-3xl">
+                <Button
+                    variant="filled"
+                    color="#0A841B"
+                    radius="xl"
+                    p="0"
+                    w="50px"
+                    h="50px"
+                >
+                    <Image src="/plus.svg" alt="Icon" width="40" height="40" />
+                </Button>
             </div>
         </div>
     );
