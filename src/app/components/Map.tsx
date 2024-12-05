07c68cebf6c7938fff62fbf0d4c08be9ab55b4a0
David Rotariu <davidrotariu139@gmail.com>
2024-12-05 20:28:13 +0200

    Implemented getting inital sensors from backend

--- src/app/components/Map.tsx
+++ src/app/components/Map.tsx
@@ -10,12 +10,18 @@ interface MapProps {
     opened: any;
     close: any;
 }
+interface sensorProps {
+    id: number;
+    lat: number;
+    lng: number;
+    name: string;
+}
 
 const Map = ({ sensor, setSensor, opened, close }: MapProps) => {
     const [map, setMap] = useState<L.Map | null>(null);
     const mapRef = useRef<HTMLDivElement>(null);
     const markerRef = useRef<L.Marker | null>(null);
-
+    const [sensors, setSensors] = useState<sensorProps[] | null>(null);
     const redIcon = new L.Icon({
         iconUrl: '/redmarker.png',
         iconSize: [32, 34],
@@ -29,65 +35,71 @@ const Map = ({ sensor, setSensor, opened, close }: MapProps) => {
         popupAnchor: [1, -34]
     });
 
-    const addMarker = () => {
-        if (map && !markerRef.current) {
-            const marker4 = L.marker([47.635, 26.24], {
-                icon: redIcon
-            }).addTo(map);
-            marker4.bindPopup('<b>Senzor 4</b>');
-            marker4.on('click', () => {
-                console.log('Marker clicked!');
-            });
-            markerRef.current = marker4;
-        }
-    };
+    // fetching the initial sensors
+    useEffect(() => {
+        const fetchSensors = async () => {
+            try {
+                const response = await fetch(
+                    'http://192.168.0.113:8000/api/sensors'
+                );
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                const data = await response.json();
+                setSensors(data.sensors);
+            } catch (error) {
+                console.error(
+                    'There was a problem with fetching the sensors:',
+                    error
+                );
+            }
+        };
+        fetchSensors();
+    }, []);
 
+    // adding and removing markers
     useEffect(() => {
         if (opened) {
-            addMarker();
+            if (map && !markerRef.current) {
+                const marker4 = L.marker([47.635, 26.24], {
+                    icon: redIcon
+                }).addTo(map);
+                marker4.bindPopup('<b>Senzor 4</b>');
+                marker4.on('click', () => {
+                    console.log('Marker clicked!');
+                });
+                markerRef.current = marker4;
+            }
         } else if (markerRef.current && map) {
             map.removeLayer(markerRef.current);
             markerRef.current = null;
         }
     }, [opened, map]);
 
+    // creating the inital map
     useEffect(() => {
-        if (mapRef.current && !map) {
+        if (mapRef.current && !map && sensors) {
             const initMap = L.map(mapRef.current).setView(
                 [47.64115437373143, 26.244929831845194],
                 14
             );
-
             L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
                 attribution:
                     '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
             }).addTo(initMap);
 
-            const marker1 = L.marker([47.64238, 26.224382], {
-                icon: blueIcon
-            }).addTo(initMap);
-            marker1.bindPopup('<b>Senzor 1</b>');
-            marker1.on('click', () => {
-                setSensor(1);
+            sensors?.forEach((marker, index) => {
+                const newMarker = L.marker([marker.lat, marker.lng], {
+                    icon: blueIcon
+                }).addTo(initMap);
+                newMarker.bindPopup(`<b>${marker.name}</b>`);
+                newMarker.on('click', () => {
+                    setSensor(index + 1);
+                });
             });
-            const marker2 = L.marker([47.64115437373143, 26.244929831845194], {
-                icon: blueIcon
-            }).addTo(initMap);
-            marker2.bindPopup('<b>Senzor 2</b>');
-            marker2.on('click', () => {
-                setSensor(2);
-            });
-            const marker3 = L.marker([47.645176, 26.255689], {
-                icon: blueIcon
-            }).addTo(initMap);
-            marker3.bindPopup('<b>Senzor 3</b>');
-            marker3.on('click', () => {
-                setSensor(3);
-            });
-
             setMap(initMap);
         }
-    }, [map]);
+    }, [map, sensors]);
 
     return <div ref={mapRef} className="w-full h-screen z-0"></div>;
 };
